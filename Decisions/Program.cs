a3dbeb4e34c4c9efe0a83eaa970b627fbaea4f3c
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-07 14:09:59 -0500

    added if statment using user input

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -6,11 +6,20 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please type something and press the Enter key");
-            string userValue;
-            userValue = Console.ReadLine();
-            Console.WriteLine("You typed: " + userValue);
-            Console.ReadLine();
+            //Console.WriteLine("Please type something and press the Enter key");
+            //string userValue;
+            //userValue = Console.ReadLine();
+            //Console.WriteLine("You typed: " + userValue);
+
+
+            Console.WriteLine("Would you prefer what is behind door number 1, 2, or 3?");
+            string userValue = Console.ReadLine();
+            if (userValue == "1")
+            {
+                Console.WriteLine("You won a new car!");
+                Console.ReadLine();
+            }
+
         }
     }
 }
