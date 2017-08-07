06d6452e4fdc409e63c118323d62f3ab9697120a
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-07 14:33:19 -0500

    added a conditional operator with a string constructor

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -12,21 +12,28 @@ namespace Decisions
             //Console.WriteLine("You typed: " + userValue);
 
 
+            //Console.WriteLine("Would you prefer what is behind door number 1, 2, or 3?");
+            //string userValue = Console.ReadLine();
+
+            //string message = "";
+
+            //if (userValue == "1")
+            //    message = "You won a new car!";
+            //else if (userValue == "2")
+            //    message = "You won a new boat!";
+            //else if (userValue == "3")
+            //    message = "You won a new cat!";
+            //else
+            //    message = "Sorry, we didn't understand. You lose!";
+
+            //Console.WriteLine(message);
+
+
             Console.WriteLine("Would you prefer what is behind door number 1, 2, or 3?");
             string userValue = Console.ReadLine();
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.WriteLine("You won a {0} and you typed {1}", message, userValue);
 
-            string message = "";
-
-            if (userValue == "1")
-                message = "You won a new car!";
-            else if (userValue == "2")
-                message = "You won a new boat!";
-            else if (userValue == "3")
-                message = "You won a new cat!";
-            else
-                message = "Sorry, we didn't understand. You lose!";
-
-            Console.WriteLine(message);
             Console.ReadLine();
         }
     }
