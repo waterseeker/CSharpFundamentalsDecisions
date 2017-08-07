7dfceb16ab7f22ea0fdf929cb0b9b64244c81110
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-07 14:22:44 -0500

    changed hard coded cwl's to using variable of message

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -14,26 +14,28 @@ namespace Decisions
 
             Console.WriteLine("Would you prefer what is behind door number 1, 2, or 3?");
             string userValue = Console.ReadLine();
+
+            string message = "";
+
             if (userValue == "1")
             {
-                Console.WriteLine("You won a new car!");
-                Console.ReadLine();
+                message = "You won a new car!";
             }
             else if (userValue == "2")
             {
-                Console.WriteLine("You won a new boat!");
-                Console.ReadLine();
+                message = "You won a new boat!";
             }
             else if (userValue == "3")
             {
-                Console.WriteLine("You won a new cat!");
-                Console.ReadLine();
+                message = "You won a new cat!";
             }
             else
             {
-                Console.WriteLine("Sorry, we didn't understand. You lose!");
-                Console.ReadLine();
+                message = "Sorry, we didn't understand. You lose!";
             }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
     }
 }
